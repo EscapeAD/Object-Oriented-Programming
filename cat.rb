d7b71526aa17a2f0a0a65429a4c8ddba06eb64ba
Adam Tse <mr.adam.tse@gmail.com>
2016-08-11 13:34:25 -0400

    Update filesystem of cat.rb

--- cat.rb
+++ cat.rb
@@ -1,12 +1,10 @@
 class Cat
-  attr_accessor :name
-  attr_accessor :preferred_food
-  attr_accessor :meal_time
+  attr_accessor :name :preferred_food :meal_time
 
   def initialize(name, preferred_food, meal_time)
     @name = name
     @preferred_food = preferred_food
-    @meal_time = meal_time.to_i
+    @meal_time = meal_time
   end
 
   def eats_at
