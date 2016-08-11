26fe0a5f4b1d9bcea72530d4045a5984a7d00672
Adam Tse <mr.adam.tse@gmail.com>
2016-08-11 16:01:35 -0400

    redefined methods, fixed issue with experience adding prematurely

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -2,34 +2,42 @@ class Paperboy
   attr_accessor :deliver
   attr_reader :quota
 
-  def initialize(name, side, quota)
+  def initialize(name, side)
     @name                   = name
     @experience             = 0
     @earnings               = 0
     @houses_delivered       = 0
-    @quota                  = quota
+    @que                    = 50
     @side                   = side
     @pay                    = 0.25
     @lost                   = 2.00
     @over                   = 0.50
   end
 
+  def quota
+    return (@que + (@experience/2))
+  end
+
   def earnings
-    total = @houses_delivered - @quota
-    if total < 0
-      @earnings     = (@houses_delivered * @pay) - (total.abs * @lost)
+    return @earnings
+  end
+  def update_earnings
+    total           = @houses_delivered - quota
+    if    total < 0
+      @earnings     += (@houses_delivered * @pay) - (@lost)
     elsif total > 0
-      @earnings     = (@quota * @pay) + (total * @over)
+      @earnings     += (quota * @pay) + (total.abs * @over)
     else
-      @earnings     = @quota * @pay
+      @earnings     += quota * @pay
     end
+
     return @earnings
   end
 
   def deliver(start_address, end_address)
 
-    total = (end_address.abs - start_address.abs)/2
-    unless total % 2 == 0
+    total = (end_address - start_address)/2.0
+    unless total % 1 == 0
       total = total.floor + 1
     end
 
@@ -38,9 +46,11 @@ class Paperboy
     elsif @side == "odd"
       @houses_delivered = total.floor
     else
-      @earnings = "Have not choose a side to deliver, even or odd"
+       return "Have not choose a side to deliver, even or odd"
     end
-    return @earnings
+    update_earnings
+    @experience += (@houses_delivered)
+    return @houses_delivered
   end
 
   def report
@@ -48,17 +58,16 @@ class Paperboy
   end
 end
 
-tommy = Paperboy.new("Tommy", "even", 50)
-boy = Paperboy.new("Tommy", "even", 50)
+tommy = Paperboy.new("Tommy", "even")
 
 
 puts tommy.quota # => 50
-tommy.deliver(101, 220) # => 17.75
+puts tommy.deliver(101, 220) # => 17.75
 puts tommy.earnings #=> 17.5
 tommy.report # => "I'm Tommy, I've delivered 60 papers and I've earned $17.5 so far!"
 
 
-puts boy.quota # => 80
-boy.deliver(1, 150) # => 18.75
-puts boy.earnings #=> 36.25
-boy.report # => "I'm Tommy, I've been delivered 134 papers and I've earned $36.25 so far!"
+puts tommy.quota # => 80
+tommy.deliver(1, 150) # => 18.75
+puts tommy.earnings #=> 36.25
+tommy.report # => "I'm Tommy, I've been delivered 134 papers and I've earned $36.25 so far!"
