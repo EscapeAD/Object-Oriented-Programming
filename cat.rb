43dfbab5570dd3b78263cefd1b70b1438532a81e
Adam Tse <mr.adam.tse@gmail.com>
2016-08-10 22:30:17 -0400

    Inital code added for cat.rb

---
+++ cat.rb
@@ -0,0 +1,38 @@
+# attr_accessor :name
+# attr_accessor :preferred_food
+# attr_accessor :meal_time
+#
+# def inintalize(:name, :preferred_food, :meal_time)
+#   @name = :name,
+#   @preferred_food = :preferred_food,
+#   @meal_time = :meal_time
+# end
+
+class Cat
+  attr_accessor :name
+  attr_accessor :preferred_food
+  attr_accessor :meal_time
+
+  def initialize(name, preferred_food, meal_time)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time.to_i
+  end
+
+  def eats_at
+    if @meal_time < 12 && @meal_time > 0
+      eat = @meal_time.to_s + " AM"
+    elsif @meal_time > 12 && @meal_time < 24
+      time = @meal_time/2
+      eat = time.to_s + " PM"
+    else
+      eat = "Invalid time"
+    end
+    return eat
+  end
+
+  def meow
+    puts "My Name is #{name} and I eat #{preferred_food} at #{eats_at}"
+  end
+
+end
