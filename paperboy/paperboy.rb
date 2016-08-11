365498520023cdd7b874053552f4754479692dab
Adam Tse <mr.adam.tse@gmail.com>
2016-08-11 14:55:01 -0400

    changing delivery method

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -2,12 +2,12 @@ class Paperboy
   attr_accessor :deliver
   attr_reader :quota
 
-  def initialize(name, side)
+  def initialize(name, side, quota)
     @name                   = name
     @experience             = 0
     @earnings               = 0
     @houses_delivered       = 0
-    @quota                  = 50
+    @quota                  = quota
     @side                   = side
     @pay                    = 0.25
     @lost                   = 2.00
@@ -17,7 +17,7 @@ class Paperboy
   def earnings
     total = @houses_delivered - @quota
     if total < 0
-      @earnings     = total.abs * @lost
+      @earnings     = (@houses_delivered * @pay) - (total.abs * @lost)
     elsif total > 0
       @earnings     = (@quota * @pay) + (total * @over)
     else
@@ -28,7 +28,7 @@ class Paperboy
 
   def deliver(start_address, end_address)
 
-    total = (end_address - start_address)/2
+    total = (end_address.abs - start_address.abs)/2
     unless total % 2 == 0
       total = total.floor + 1
     end
@@ -48,9 +48,17 @@ class Paperboy
   end
 end
 
-tommy = Paperboy.new("Tommy", "even")
+tommy = Paperboy.new("Tommy", "even", 50)
+boy = Paperboy.new("Tommy", "even", 50)
+
 
 puts tommy.quota # => 50
 tommy.deliver(101, 220) # => 17.75
 puts tommy.earnings #=> 17.5
-puts tommy.report # => "I'm Tommy, I've delivered 60 papers and I've earned $17.5 so far!"
+tommy.report # => "I'm Tommy, I've delivered 60 papers and I've earned $17.5 so far!"
+
+
+puts boy.quota # => 80
+boy.deliver(1, 150) # => 18.75
+puts boy.earnings #=> 36.25
+boy.report # => "I'm Tommy, I've been delivered 134 papers and I've earned $36.25 so far!"
